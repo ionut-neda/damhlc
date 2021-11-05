bdb642867d43cf26d8eb5693b699d358786341a4
rafa2m <rafaelmendez@iesantoniogala.es>
2021-11-05 09:07:17 +0100

    Primer commit del repositorio

---
+++ TEMA1/estatico.php
@@ -0,0 +1,27 @@
+<?php
+function contar()
+{
+    static $a = 0;
+    echo "$a<br>";
+    $a++;
+}
+
+function contar_recursivo()
+{
+    static $count = 0;
+
+    $count++;
+    echo "$count<br>";
+    if ($count < 10) {
+        contar_recursivo();
+    }
+    $count--;
+    echo "$count<br>";
+}
+//contar(); //mostrará el valor 0 de $a
+
+//contar(); //mostrará el valor 1 de $a
+
+contar_recursivo();
+
+?>
---
+++ TEMA1/iterable.php
@@ -0,0 +1,18 @@
+<?php    
+    //inicialización de un iterable
+    //a partir de un array asociativo
+    $iterable=array("uno" => 1,
+    "dos" => 2,
+    "tres" => 3,
+    "cuatro" => 4);
+    
+    //recorrer el iterable y mostrar su contenido
+    foreach($iterable as $valor){
+        echo "$valor<br>";
+    }
+
+    //muestro un elemento del iterable
+    //a partir de su clave
+    echo $iterable["dos"];
+
+?>
